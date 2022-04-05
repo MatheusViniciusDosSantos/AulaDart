ae73ab425e3b4ae733aa5fb88cc9e3547a29a333
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2022-04-05 20:27:38 -0300

    Comentado primeira função

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -2,6 +2,6 @@ import 'package:dart_application/dart_application.dart' as dart_application;
 import 'package:dart_application/primeiro.dart' as primeiro;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application.calculate()}!');
+  //print('Hello world: ${dart_application.calculate()}!');
   primeiro.minhaFuncao();
 }
