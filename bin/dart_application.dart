ea708ec6da71c8a5e4207eff0bd174f7caa6c150
MatheusViniciusDosSantos <matheus.vinicius77@hotmail.com>
2022-04-05 20:26:32 -0300

    Minha função

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -1,5 +1,7 @@
 import 'package:dart_application/dart_application.dart' as dart_application;
+import 'package:dart_application/primeiro.dart' as primeiro;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application.calculate()}!');
+  primeiro.minhaFuncao();
 }
---
+++ lib/primeiro.dart
@@ -0,0 +1,3 @@
+void minhaFuncao() {
+  print("Boa noite");
+}
